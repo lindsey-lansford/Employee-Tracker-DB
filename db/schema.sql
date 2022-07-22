c22cb200cbadaea22bc0498614dc0b4ed153e825
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-07-22 14:13:57 -0600

    refactoring schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,34 +3,28 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE departments(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE departments (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE
 );
 
-CREATE TABLE roles(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL
+CREATE TABLE roles (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL UNIQUE,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX dept_idx (department_id),
+    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employees(
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE employees (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employees(id)
-    ON DELETE SET NULL
+    role_id INT UNSIGNED NOT NULL,
+    INDEX role_idx (role_id),
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    manager_id INT UNSIGNED,
+    INDEX mgr_idx (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,33 @@
 INSERT INTO departments (name)
-VALUES ("Accounting"),
+VALUES 
+    ("Finance"),
     ("Sales"),
     ("Engineering"),
-    ("Marketing"),
     ("Human Resources");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Accounting Manager", 75000, 1),
-("Sales Manager", 73000, 2),
-("Software Engineer", 90000, 3),
-("Marketing Manager", 60000, 4),
-("HR Manager", 70000, 5);
+VALUES 
+    ("Payroll Manager", 75000, 1),
+    ("Payroll Clerk", 50000, 1),
+    ("Finance Assistant", 53000, 1),
+    ("Sales Manager", 73000, 2),
+    ("Senior Sales Associate", 66000, 2),
+    ("Sales Associate", 60000, 2),
+    ("Senior Engineering Manager", 110000, 3),
+    ("Software Engineer", 90000, 3),
+    ("Junior Engineer", 85000, 3),
+    ("HR Assistant", 58000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Ham", 1, 1),
-("Jennifer", "Lopez", 2, 2),
-("Amy", "Poeler", 3, 3),
-("Will", "Ferrell", 4, 4),
-("Adam", "Sandler", 4, 5);
+VALUES 
+    ("John", "Ham", 1, NULL),
+    ("Jennifer", "Lopez", 2, 1),
+    ("Amy", "Poeler", 3, 1),
+    ("Will", "Ferrell", 4, NULL),
+    ("Adam", "Sandler", 5, 4),
+    ("Brad", "Pitt", 6, 4),
+    ("Tom", "Cruise", 6, 4),
+    ("Kris", "Jenner", 7, NULL),
+    ("Miles", "Teller", 8, 8),
+    ("Margot", "Robbie", 9, 8),
+    ("Kanye", "West", 10, NULL);
