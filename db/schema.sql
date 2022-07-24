5bf526ec5369cf29f80fde29c077f4778a18e1c1
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-07-23 19:24:23 -0600

    final final edits

--- db/schema.sql
+++ db/schema.sql
@@ -22,6 +22,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-    manager_id INT,
+    manager_id INT UNSIGNED,
     FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
